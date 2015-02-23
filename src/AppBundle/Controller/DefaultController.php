32301bfc1e8c06583ffa7b4c75f5ff7450a0e414
Carlos Salvatierra <cslucano@gmail.com>
2015-02-22 22:49:00 -0500

    Added Contact CRUD and Templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
+use AppBundle\Form\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,5 +17,193 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contacts = $em->getRepository('AppBundle:Contact')->findAll();
+
+        return $this->render(
+            'default/dashboard.html.twig',
+            [
+                'contacts' => $contacts,
+            ]
+        );
+    }
+
+    /**
+     * Creates a new Contact entity.
+     *
+     * @Route("/contact", name="contact_create")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createCreateForm($contact);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('dashboard'));
+        }
+
+        return $this->render(
+            'default/contact_new.html.twig',
+            [
+                'contact' => $contact,
+                'form'   => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * Creates a form to create a Contact entity.
+     *
+     * @param Contact $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Contact $entity)
+    {
+        $form = $this->createForm(new ContactType(), $entity, array(
+            'action' => $this->generateUrl('contact_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Contact entity.
+     *
+     * @Route("/contact/new", name="contact_new")
+     * @Method("GET")
+     */
+    public function newAction()
+    {
+        $contact = new Contact();
+        $form   = $this->createCreateForm($contact);
+
+        return $this->render(
+            'default/contact_new.html.twig',
+            [
+                'contact' => $contact,
+                'form'   => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Contact entity.
+     *
+     * @Route("/contact/{id}/edit", name="contact_edit")
+     * @Method("GET")
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contact = $em->getRepository('AppBundle:Contact')->find($id);
+
+        if (!$contact) {
+            throw $this->createNotFoundException('Unable to find Contact entity.');
+        }
+
+        $form = $this->createEditForm($contact);
+
+
+        return $this->render(
+            'default/contact_edit.html.twig',
+            [
+                'contact'      => $contact,
+                'form'   => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * Creates a form to edit a Contact entity.
+     *
+     * @param Contact $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Contact $entity)
+    {
+        $form = $this->createForm(new ContactType(), $entity, array(
+            'action' => $this->generateUrl('contact_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Contact entity.
+     *
+     * @Route("/contact/{id}", name="contact_update")
+     * @Method("PUT")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contact = $em->getRepository('AppBundle:Contact')->find($id);
+
+        if (!$contact) {
+            throw $this->createNotFoundException('Unable to find Contact entity.');
+        }
+
+        $editForm = $this->createEditForm($contact);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('dashboard'));
+        }
+
+        return $this->render(
+            'default/contact_edit.html.twig',
+            [
+                'contact'      => $contact,
+                'edit_form'   => $editForm->createView(),
+            ]
+        );
+    }
+
+    /**
+     * Deletes a Contact entity.
+     *
+     * @Route("/contact/{id}/delete", name="contact_delete")
+     * @Method("GET")
+     */
+    public function deleteAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $contact = $em->getRepository('AppBundle:Contact')->find($id);
+
+        if (!$contact) {
+            throw $this->createNotFoundException('Unable to find Contact entity.');
+        }
+
+        $em->remove($contact);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('dashboard'));
     }
 }
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone_number", type="string", length=255)
+     */
+    private $phoneNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="notes", type="text")
+     */
+    private $notes;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set phoneNumber
+     *
+     * @param string $phoneNumber
+     * @return Contact
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneNumber
+     *
+     * @return string 
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param string $notes
+     * @return Contact
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return string 
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+}
